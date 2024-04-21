927ab1ae7758caed35c3a5ac37067f05318f1dd7
Victorya19 <vikaeelent@gmail.com>
2024-04-21 11:46:58 +0300

    Обработка ошибки вычисления корня из отрицательного числа

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,9 +10,10 @@ class Calculator
         Console.WriteLine("2 - Вычитание");
         Console.WriteLine("3 - Умножение");
         Console.WriteLine("4 - Деление");
+        Console.WriteLine("5 - Квадратный корень");
 
         int choice;
-        if (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4)
+        if (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5)
         {
             Console.WriteLine("Неверный выбор операции.");
             return;
@@ -26,34 +27,59 @@ class Calculator
             return;
         }
 
-        Console.WriteLine("Введите второе число:");
-        double num2;
-        if (!double.TryParse(Console.ReadLine(), out num2))
-        {
-            Console.WriteLine("Неверный формат числа.");
-            return;
-        }
-
         double result = 0;
 
         switch (choice)
         {
             case 1:
+                Console.WriteLine("Введите второе число:");
+                double num2;
+                if (!double.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine("Неверный формат числа.");
+                    return;
+                }
                 result = num1 + num2;
                 break;
             case 2:
+                Console.WriteLine("Введите второе число:");
+                if (!double.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine("Неверный формат числа.");
+                    return;
+                }
                 result = num1 - num2;
                 break;
             case 3:
+                Console.WriteLine("Введите второе число:");
+                if (!double.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine("Неверный формат числа.");
+                    return;
+                }
                 result = num1 * num2;
                 break;
             case 4:
+                Console.WriteLine("Введите второе число:");
+                if (!double.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine("Неверный формат числа.");
+                    return;
+                }
                 if (num2 == 0)
                 {
                     Console.WriteLine("Деление на ноль невозможно. Попробуйте еще раз.");
                     return;
                 }
                 result = num1 / num2;
+                break;
+            case 5:
+                if (num1 < 0)
+                {
+                    Console.WriteLine("Извлечение квадратного корня из отрицательного числа невозможно.");
+                    return;
+                }
+                result = Math.Sqrt(num1);
                 break;
             default:
                 Console.WriteLine("Неверный выбор операции.");
