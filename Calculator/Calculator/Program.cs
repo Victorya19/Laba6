2204f261b9b24ecc58a24d0a81cab69081daaa3f
Victorya19 <vikaeelent@gmail.com>
2024-04-21 11:34:44 +0300

    Обработка ошибки неверного формата числа

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -5,19 +5,34 @@ class Calculator
     static void Main(string[] args)
     {
         Console.WriteLine("Простой калькулятор");
-        Console.WriteLine("Выберите операцию:" );
+        Console.WriteLine("Выберите операцию:");
         Console.WriteLine("1 - Сложение");
         Console.WriteLine("2 - Вычитание");
         Console.WriteLine("3 - Умножение");
         Console.WriteLine("4 - Деление");
 
-        int choice = Convert.ToInt32(Console.ReadLine());
+        int choice;
+        if (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4)
+        {
+            Console.WriteLine("Неверный выбор операции.");
+            return;
+        }
 
         Console.WriteLine("Введите первое число:");
-        double num1 = Convert.ToDouble(Console.ReadLine());
+        double num1;
+        if (!double.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("Неверный формат числа.");
+            return;
+        }
 
         Console.WriteLine("Введите второе число:");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        double num2;
+        if (!double.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Неверный формат числа.");
+            return;
+        }
 
         double result = 0;
 
@@ -33,11 +48,12 @@ class Calculator
                 result = num1 * num2;
                 break;
             case 4:
-                result = num1 / num2;
                 if (num2 == 0)
-                    {
+                {
                     Console.WriteLine("Деление на ноль невозможно. Попробуйте еще раз.");
+                    return;
                 }
+                result = num1 / num2;
                 break;
             default:
                 Console.WriteLine("Неверный выбор операции.");
